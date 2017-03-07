be308a3e285bb5efc8ee3b758df27d1cdce46f5a
LeraKulakova <lera_kulakova_1997@mail.ru>
2017-03-07 13:43:33 +0600

    Реолизовал классы

---
+++ Java/model/Passport.java
@@ -0,0 +1,10 @@
+package model;
+
+import java.util.Date;
+
+class Passport {
+    Integer seriya;
+    Integer nomer;
+    Date vidachi;
+    String podrazdeleniya;
+}
---
+++ Java/model/Sotrudnik.java
@@ -0,0 +1,13 @@
+package model;
+
+import java.util.Date;
+
+public class Sotrudnik {
+    Integer id;
+    String fio;
+    Date rozhdenie;
+    Integer pol;
+    String telefon;
+    Passport passport;
+    Dolzhnost dolzhnost;
+}
